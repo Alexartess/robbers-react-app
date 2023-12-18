a5d94f402a4977accf93987dc5270020261ff4e6
Alexartess <alexandraalesina@gmail.com>
2023-12-18 19:30:21 +0300

    fix: deleted strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import i18n from './i18n'
 import { I18nextProvider } from 'react-i18next'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <I18nextProvider i18n={i18n}>
     <App />
     </I18nextProvider>
-  </React.StrictMode>,
+  
 )
