2fb70ef410bb70cdc407afdf2731f2e0c130e18b
Alexartess <alexandraalesina@gmail.com>
2023-12-19 01:40:13 +0300

    fix: hope gh pages will work

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import jsonData from './data.json'
-
+import jsonData from "./data.json";
 
 import MainPage from "./MainPage";
 import ListOfRob from "./ListOfRob";
@@ -8,30 +7,26 @@ import ListOfRob from "./ListOfRob";
 import Robber from "./Robber";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import ScrollToAnchor from "./Components/ScrollToAnchor";
 
 function App() {
-  
-
   return (
     <>
-     {/* basename="/robbers-react-app" */}
-     <BrowserRouter >
-     <Routes>
-      <Route index element={<MainPage/>}/>
-      <Route path="/home" element={<MainPage/>}/>
-      <Route path="/robbers" element={<ListOfRob/>}/>
-      <Route path="/James" element={<Robber rob_index="0"/>}/>
-      <Route path="/Cassidy" element={<Robber rob_index="1"/>}/>
-      <Route path="/Dillinger" element={<Robber rob_index="2"/>}/>
-      <Route path="/Gillis" element={<Robber rob_index="3"/>}/>
-      <Route path="/Kid" element={<Robber rob_index="4"/>}/>
-      </Routes>
-      
+      <BrowserRouter basename="/robbers-react-app">
+        <Routes>
+          <Route index element={<MainPage />} />
+          <Route path="/home" element={<MainPage />} />
+          <Route path="/robbers" element={<ListOfRob />} />
+          <Route path="/James" element={<Robber rob_index="0" />} />
+          <Route path="/Cassidy" element={<Robber rob_index="1" />} />
+          <Route path="/Dillinger" element={<Robber rob_index="2" />} />
+          <Route path="/Gillis" element={<Robber rob_index="3" />} />
+          <Route path="/Kid" element={<Robber rob_index="4" />} />
+        </Routes>
+        <ScrollToAnchor />
       </BrowserRouter>
-     
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
